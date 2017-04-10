72892140321d57455752e385b9cabcaec33b15f9
Daniel Shin <shin.daniel.d@gmail.com>
2017-04-10 15:09:02 -0600

    initial model tests

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Chat
 
-# Create your tests here.
+class ChatTestCase(TestCase):
+    def setUp(self):
+        Chat.objects.create(username="test_1", text="text 1")
+        Chat.objects.create(username="test_1", text="text 2")
+        Chat.objects.create(username="test_2", text="text 3")
+
+    # Model Tests
+    def test_chats_exist(self):
+        self.assertEqual(Chat.objects.count(), 3)
+
+    def test_get_texts_returns_values(self):
+        user_1 = Chat.objects.filter(username="test_1").first()
+        expected = [
+            {'id': user_1.id, 'text': user_1.text},
+            {'id': user_1.id, 'text': user_1.text}
+        ]
+
+    
