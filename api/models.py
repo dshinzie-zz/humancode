99d8854fe6a359f109fbb4970140ddda552e6cdf
Daniel Shin <shin.daniel.d@gmail.com>
2017-04-10 12:43:02 -0600

    initial model setup

--- api/models.py
+++ api/models.py
@@ -1,5 +1,9 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Room(models.Model):
+    name = models.CharField(max_length=200)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(blank=True, null=True)
